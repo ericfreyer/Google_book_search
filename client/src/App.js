a99830d94c2e1d73e56e8b2a34dccd1aeee3008f
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-04-12 11:16:07 -0400

    almost done with components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBooks from './pages/SearchBooks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component { 
+  render () {
+    return(
+      <Router>
+        <div>
+          <NavBar />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/search" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  };
+};
 
 export default App;
